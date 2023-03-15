6adfc7026722367c7291123c712c6375c3b9fd46
samu3ak <samuelbolivar2003@gmail.com>
2023-03-15 12:49:59 +0100

    express-session seteado, configuración de views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// Parámetros establecidos para las opciones de renderizado de la página
+const params = {
+  title: "YGDB - Inicio"
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: params.title });
 });
 
 module.exports = router;
