b149328c9cc5a4ea2db6e7dd6d6ac93057833894
samu3ak <samuelbolivar2003@gmail.com>
2023-03-16 21:05:55 +0100

    bs5 añadido + logo + header nav
    Se ha añadido Bootstrap 5 con el estilo Minty de Bootswatch --> https://bootswatch.com/minty/
    
    Se ha incluido la barra de navegación
    
    Además, se ha añadido el logo definitivo de YGBD e incluido en la barra de navegación de la aplicación

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,22 @@ var router = express.Router();
 
 // Parámetros establecidos para las opciones de renderizado de la página
 const params = {
-  title: "YGDB - Inicio"
+  title: "YGDB - Inicio",
+  inicioActivo: "active"
 };
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: params.title });
-  req.getConnection((err, connection) => {
-    if (err) return next(err);
+  res.render('index', { title: params.title, inicioActivo: params.inicioActivo });
+  // req.getConnection((err, connection) => {
+  //   if (err) return next(err);
 
-    connection.query("SELECT * FROM usuario", (err, rows)=>{
-      if (err) return next(err);
+  //   connection.query("SELECT * FROM usuario", (err, rows)=>{
+  //     if (err) return next(err);
 
-      console.log(rows);
-    });
-  });
+  //     console.log(rows);
+  //   });
+  // });
 });
 
 module.exports = router;
