1a5e39ba1e95c85eea3ba3814873a3100029a24a
samu3ak <samuelbolivar2003@gmail.com>
2023-05-10 16:56:30 +0200

    Comprobación permisos usuario para herramientas admin

--- controllers/gestionUsuariosController.js
+++ controllers/gestionUsuariosController.js
@@ -6,8 +6,16 @@ const params = {
     title: "YGDB - Administrador - Gestión de usuarios"
 };
 
-exports.gestionUsuarios = async (req, res) => {
+exports.gestionUsuarios = async (req, res, next) => {
+    const usuarioLogueado = req.session.usuario ? req.session.usuario : null;
     // Recogida de usuarios registrados con rol de usuario
-    const Users = await Usuario.findAll({ where: { rol: "usuario" }, raw: true }); 
-    res.render("gestionUsuarios", { title: params.title, usuario: req.session.usuario, usuarios: Users });
+    const Users = await Usuario.findAll({ where: { rol: "usuario" }, raw: true });
+
+    // Comprobación permisos y renderizado
+    if (usuarioLogueado !== null && usuarioLogueado.rol === "administrador") {
+        res.render("gestionUsuarios", { title: params.title, usuario: usuarioLogueado, usuarios: Users });
+    } else {
+        params.title = "YGDB - Error";
+        res.render("errorPermisos", { title: params.title });
+    }
 };
