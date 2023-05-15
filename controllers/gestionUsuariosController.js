e091bfa8680516a2f2fd743a885218a686f6299c
samu3ak <samuelbolivar2003@gmail.com>
2023-05-15 21:53:56 +0200

    Añadida barra de búsqueda por nombre usuario a la gestión de usuarios

--- controllers/gestionUsuariosController.js
+++ controllers/gestionUsuariosController.js
@@ -3,17 +3,18 @@ const [Usuario, sequelize] = require("../models/cargarModelos").getModelSequeliz
 
 // Parámetros establecidos para las opciones de renderizado de la página
 const params = {
-    title: "YGDB - Administrador - Gestión de usuarios"
+    title: "YGDB - Administrador - Gestión de usuarios",
+    errorMsg: ""
 };
 
 exports.gestionUsuarios = async (req, res, next) => {
     const usuarioLogueado = req.session.usuario ? req.session.usuario : null;
+    var busqueda = req.query.nombreUsuario ? req.query.nombreUsuario : "";
     // Recogida de usuarios registrados con rol de usuario
-    const Users = await Usuario.findAll({ where: { rol: "usuario" }, raw: true });
-
+    var Users = await sequelize.query("SELECT id_usuario, nombreUsuario, correo, rol, numAmigos FROM usuario WHERE rol = 'usuario' AND LOWER(nombreUsuario) LIKE ?", { replacements: [`%${busqueda.toLowerCase()}%`], type: sequelize.QueryTypes.SELECT });
     // Comprobación permisos y renderizado
     if (usuarioLogueado !== null && usuarioLogueado.rol === "administrador") {
-        res.render("gestionUsuarios", { title: params.title, usuario: usuarioLogueado, usuarios: Users });
+        res.render("gestionUsuarios", { title: params.title, usuario: usuarioLogueado, usuarios: Users, nombreUsuario: busqueda });
     } else {
         params.title = "YGDB - Error";
         res.render("errorPermisos", { title: params.title });
