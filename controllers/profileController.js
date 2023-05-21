8c7fbfcd738be7f91751611c62998e218d294cce
Samuel Bolívar Villanueva <samuelbolivar2003@gmail.com>
2023-05-21 18:32:43 +0200

    Cambiada vista de exploración de usuarios

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,3 +1,6 @@
+const { QueryTypes } = require("sequelize");
+const sequelize = require("../models/cargarModelos").getSequelize();
+
 // Parámetros establecidos para las opciones de renderizado de la página
 const params = {
     title: "YGDB - Perfil de "
@@ -5,4 +8,17 @@ const params = {
 
 exports.profile = (req, res) => {
     res.render("profile", { title: params.title + `${req.session.usuario.nombreUsuario}`, usuario: req.session.usuario });
+};
+
+exports.profileUser = async (req, res) => {
+    const username = req.params.username;
+
+    let usuario = await sequelize.query("SELECT * FROM usuario WHERE nombreUsuario = ?", { replacements: [username], type: sequelize.QueryTypes.SELECT });
+    if (usuario) {
+        usuario = usuario[0];
+        res.render("profile", { title: params.title + `${username}`, usuario: usuario });
+    } else {
+        res.render("error");
+    }
+
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 var profileController = require("../controllers/profileController.js");
 const md_auth = require("../middlewares/auth");
 
 /* GET home page. */
 router.get('/', [md_auth.userLogged], profileController.profile);
+
+router.get('/:username', profileController.profileUser);
 
 module.exports = router;
