e06448a91bdb6c417a0d5d3310d1184ad2a4234b
Samuel Bolívar Villanueva <samuelbolivar2003@gmail.com>
2023-05-28 12:39:34 +0200

    Arreglado notificaciones de solicitud solo en estado pendiente

--- services/notification.js
+++ services/notification.js
@@ -2,7 +2,7 @@ const { QueryTypes } = require("sequelize");
 const sequelize = require("../models/cargarModelos").getSequelize();
 
 async function cargarSolicitudesUsuario(userId) {
-    const query = await sequelize.query("SELECT * FROM usuario u WHERE u.id_usuario = (SELECT a.id_usuario_usuarioAmigo FROM usuarioamigo a WHERE a.id_usuario_usuarioAmigo = u.id_usuario AND a.id_usuario2_usuarioAmigo = ?);",
+    const query = await sequelize.query("SELECT * FROM usuario u WHERE u.id_usuario = (SELECT a.id_usuario_usuarioAmigo FROM usuarioamigo a WHERE a.estado = 'pendiente' AND a.id_usuario_usuarioAmigo = u.id_usuario AND a.id_usuario2_usuarioAmigo = ?);",
         {
             replacements: [
                 userId],
