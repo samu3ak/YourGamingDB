9a4e4c10214697a3ee312ee539d5f456f2387643
samu3ak <samuelbolivar2003@gmail.com>
2023-05-30 16:28:39 +0200

    Login necesario para ver perfil propio añadido

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 var profileController = require("../controllers/profileController.js");
+const md_auth = require("../middlewares/auth");
 
 /* GET home page. */
-router.get('/', profileController.profile);
+router.get('/', [md_auth.userLogged], profileController.profile);
 
 router.get('/:username', profileController.profileUser);
 
